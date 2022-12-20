4e2a4313849906e647ef9d8eb5f49c56f115d064
Ethan Dodgen <thefivalco@gmail.com>
2022-12-20 14:19:21 -0600

    added stream api connection

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'uq93t7zbvaby'
+
+const App = () => {
+  return (
+    <div className='app__wrapper'>
+        <Chat>
+            
+        </Chat>
+    </div>
+  )
+}
+
+export default App
